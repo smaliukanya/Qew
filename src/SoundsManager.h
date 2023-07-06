d2c18e2dd2033b1c8108eb277fc1c330bc99dd99
smaliukanya <ane4ka.sm@mail.ru>
2023-07-06 17:29:23 +0300

    sounds manager

--- src/SoundsManager.h
+++ src/SoundsManager.h
@@ -2,52 +2,77 @@
 
 using namespace sf;
 
-class SoundManager{
+class SoundManager {
 private:
     Music music;
     SoundBuffer gameOver;
+    SoundBuffer hitBuffer;
     SoundBuffer soundBuffer;
     SoundBuffer heartBuffer;
-    Sound endGame;
+    SoundBuffer diedSoundBuffer;
+    SoundBuffer jumpBuffer;
+    Sound jumpSound;
+    Sound diedSound;
+    Sound endGameSound;
     Sound sound;
     Sound heart;
+    Sound hit;
 public:
-    SoundManager(){
+    SoundManager() {
         soundBuffer.loadFromFile("sounds/coin.wav");
         sound.setBuffer(soundBuffer);
-        
+
         heartBuffer.loadFromFile("sounds/heart.ogg");
         heart.setBuffer(heartBuffer);
 
+        hitBuffer.loadFromFile("sounds/hit.ogg");
+        hit.setBuffer(hitBuffer);
+
         gameOver.loadFromFile("sounds/GameOver.ogg");
-        endGame.setBuffer(gameOver);
+        endGameSound.setBuffer(gameOver);
+
+        diedSoundBuffer.loadFromFile("sounds/died.ogg");
+        diedSound.setBuffer(diedSoundBuffer);
+
+        jumpBuffer.loadFromFile("sounds/jump.ogg");
+        jumpSound.setBuffer(jumpBuffer);
     }
     void playBackgroundMusic() {
         music.openFromFile("sounds/music.ogg");
-        music.setVolume(20); 
+        music.setVolume(20);
         music.setLoop(true);
         music.play();
     }
     void endBackgroundMusic() {
         music.stop();
     }
-    void playCoinSound() { 
+    void playCoinSound() {
         sound.setVolume(100);
         sound.play();
+    }
+    void playJumpSound() {
+        jumpSound.setVolume(30);
+        jumpSound.play();
+    }
+    void playHitSound() {
+        hit.setVolume(100);
+        hit.play();
     }
     void playHeartSound() {
         heart.setVolume(100);
         heart.play();
     }
-    void playGameOverSound() {
-        endGame.setVolume(100);
-        endGame.play();
+    void playDiedSound() {
+        diedSound.setVolume(100);
+        diedSound.play();
     }
-    int playingSound() {
-        if (endGame.getStatus() == sf::Sound::Playing)
-        {
-            return 1;
-        }
-        else break;
+    void playGameOverSound() {
+        endGameSound.setLoop(false);
+        endGameSound.setVolume(100);
+        if (endGameSound.getStatus() == sf::Sound::Playing) {}
+        else { endGameSound.play(); }
+    }
+    void stopGameOverSound() {
+        endGameSound.stop();
     }
 };
